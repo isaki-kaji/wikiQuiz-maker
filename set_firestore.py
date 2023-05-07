0e1454a9864c315b4cd8a52393ef2825ba28277f
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-05-07 22:45:35 +0900

    update

--- set_firestore.py
+++ set_firestore.py
@@ -1,6 +1,7 @@
 from firebase_admin import firestore
 import firebase_admin
 from firebase_admin import credentials
-cred = credentials.Certificate("〇〇.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+def set_firestore(json_name):
+    cred = credentials.Certificate(json_name)
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
