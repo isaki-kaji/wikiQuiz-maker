88bf2209b0e9499dff5dc1a84aa315c481b0ee50
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-05-07 22:43:53 +0900

    update

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,43 +1,22 @@
-#完成版が見つからない
-
 import wikipedia
 import re
 import MeCab
 import neologdn
+wikipedia.set_lang("ja")
 
-quiz_name=""
-
-sin_text=[]
-final_text=[]
-
-wiki_text=wikipedia.page(quiz_name).content
-wiki_text=wiki_text.strip().split("\n")
-for i in wiki_text:
-    nihongo=re.sub(r"[\a-zA-Z0-9_]","",i)
-    if len(nihongo)>80:
-        sin_text.append(i)
-tagger=MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd/')
-for texts in sin_text:
-    keywords=set()
-    new_keywords=set()
-    result=tagger.parse(texts)
-    sakumon=result.split("\n")
-    target="名詞"
-    for words in sakumon:
-        if "固有名詞" in words:
-            idx=words.find(target)
-            words=words[:idx]
-            words=words.replace("\t","")
-            keywords.add(words)
-#クイズの名前が空欄にならないようにする。    
-    for i in keywords:
-        if len({*i}&{*quiz_name})<2:
-            new_keywords.add(i)
-    for key in new_keywords:
-        texts=texts.replace(key,"{"+key+"}")
-    keywords.clear()
-    new_keywords.clear()
-    final_text.append(texts)
-print(*final_text,sep="\n\n")  
-
-#できてますか?
+def set_document(title_list,category,min_text_len):
+    tagger=MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd/')
+    for title in title_list:
+        add_text_list=[]
+        text_list=wikipedia.page(title).content.strip().split("\n")
+        for text in text_list:
+            text=re.sub(r'[\(\（][^()\（\）]*[\)\）]', '', text)
+            nihongo_text=re.sub(r"[\a-zA-Z0-9_]","",text)
+            if(len(nihongo_text)>min_text_len):
+                add_text_list.append(text)
+        for re_text in add_text_list:
+            key_words=set()
+            parsed_text=tagger.parse("re_text")
+            parsed_words=parsed_text.split("\n")
+            for word in parsed_words:
+                
